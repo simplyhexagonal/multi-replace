188309ab3627970498f643eb8fa3c62c71a4c303
Jean M. Lescure <jlescure@westmonroe.com>
2023-09-23 14:06:08 -0600

    chore: migrate back to npm

--- dist/multi-replace.d.ts
+++ dist/multi-replace.d.ts
@@ -1,8 +1,8 @@
 export { version } from '../package.json';
-export declare type MultiReplaceMatcher = string | RegExp;
-export declare type MultiReplaceReplacement = string | ((substring: string, ...args: any[]) => Promise<string>);
-export declare type MultiReplaceSyncReplacement = string | ((substring: string, ...args: any[]) => string);
-export declare type MultiReplacePatterns = [MultiReplaceMatcher, MultiReplaceReplacement][];
-export declare type MultiReplaceSyncPatterns = [MultiReplaceMatcher, MultiReplaceSyncReplacement][];
+export type MultiReplaceMatcher = string | RegExp;
+export type MultiReplaceReplacement = string | ((substring: string, ...args: any[]) => Promise<string>);
+export type MultiReplaceSyncReplacement = string | ((substring: string, ...args: any[]) => string);
+export type MultiReplacePatterns = [MultiReplaceMatcher, MultiReplaceReplacement][];
+export type MultiReplaceSyncPatterns = [MultiReplaceMatcher, MultiReplaceSyncReplacement][];
 export declare const multiReplace: (content: string | Promise<string>, replacePatterns: MultiReplacePatterns) => Promise<string>;
 export declare const multiReplaceSync: (content: string, replacePatterns: MultiReplaceSyncPatterns) => string;
--- dist/multi-replace.js
+++ dist/multi-replace.js
@@ -1,14 +1,22 @@
+"use strict";
 var MultiReplace = (() => {
   var __defProp = Object.defineProperty;
-  var __markAsModule = (target) => __defProp(target, "__esModule", { value: true });
-  var __require = typeof require !== "undefined" ? require : (x) => {
-    throw new Error('Dynamic require of "' + x + '" is not supported');
-  };
+  var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+  var __getOwnPropNames = Object.getOwnPropertyNames;
+  var __hasOwnProp = Object.prototype.hasOwnProperty;
   var __export = (target, all) => {
-    __markAsModule(target);
     for (var name in all)
       __defProp(target, name, { get: all[name], enumerable: true });
   };
+  var __copyProps = (to, from, except, desc) => {
+    if (from && typeof from === "object" || typeof from === "function") {
+      for (let key of __getOwnPropNames(from))
+        if (!__hasOwnProp.call(to, key) && key !== except)
+          __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+    }
+    return to;
+  };
+  var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
 
   // src/index.ts
   var src_exports = {};
@@ -23,23 +31,34 @@ var MultiReplace = (() => {
 
   // src/index.ts
   var multiReplace = async (content, replacePatterns) => {
-    const promise = replacePatterns.reduce(async (a, [matcher, replacement]) => {
-      const prevReplaceResult = await a;
-      if (typeof replacement === "string") {
-        return Promise.resolve(prevReplaceResult.replace(matcher, replacement));
-      }
-      let c = [];
-      prevReplaceResult.replace(matcher, (substring, ...args) => {
-        c.push(replacement(substring, ...args));
-        return "";
-      });
-      const result = (await Promise.all(c)).reduce((a2, b, i, arr) => a2.replace(matcher, () => arr.shift()), prevReplaceResult);
-      return Promise.resolve(result);
-    }, Promise.resolve(content));
+    const promise = replacePatterns.reduce(
+      // @ts-ignore
+      async (a, [matcher, replacement]) => {
+        const prevReplaceResult = await a;
+        if (typeof replacement === "string") {
+          return Promise.resolve(prevReplaceResult.replace(matcher, replacement));
+        }
+        let c = [];
+        prevReplaceResult.replace(matcher, (substring, ...args) => {
+          c.push(replacement(substring, ...args));
+          return "";
+        });
+        const result = (await Promise.all(c)).reduce(
+          (a2, b, i, arr) => a2.replace(matcher, () => arr.shift()),
+          prevReplaceResult
+        );
+        return Promise.resolve(result);
+      },
+      Promise.resolve(content)
+    );
     return await promise;
   };
-  var multiReplaceSync = (content, replacePatterns) => replacePatterns.reduce((a, b) => a.replace(...b), content);
-  return src_exports;
+  var multiReplaceSync = (content, replacePatterns) => replacePatterns.reduce(
+    // @ts-ignore
+    (a, b) => a.replace(...b),
+    content
+  );
+  return __toCommonJS(src_exports);
 })();
 //# sourceMappingURL=multi-replace.js.map
 'undefined'!=typeof module&&(module.exports=MultiReplace),'undefined'!=typeof window&&(MultiReplace=MultiReplace);
